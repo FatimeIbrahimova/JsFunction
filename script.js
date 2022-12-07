022fb8279571a80cbd388ef269b2f3bdb7225b06
FatimeIbrahimova <fatime.ibrahimova44@gmail.com>
2022-12-07 13:38:23 +0400

    functions

---
+++ script.js
@@ -0,0 +1,43 @@
+// function checkString(word){
+//     let count=0;
+//     for(let i=0;i<=word.length;i++){
+//         if(isNumber(word[i])){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// function isNumber(char){
+//     let charNum=parseInt(char);
+//     if(isNaN!==charNum){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+// console.log(checkString("salam"));
+
+// function checkString(word){
+//     let count=0;
+//     for(let i=0;i<=word.length;i++){
+//         if(isNumber(word[i])){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// function isNumber(char){
+//     let charNum=parseInt(char);
+//     if(isNaN!==charNum && char !=" "){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+// console.log(checkString("salam fatime"));  
+
+
